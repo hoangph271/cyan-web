69668b876504e71dc78916a0d95318756598e1e8
Huy Hoàng Phan <hoangph271@gmail.com>
2019-10-04 01:11:40 +0700

    💩 Fix deadcode, login fix setSate after unmounted

--- src/App.js
+++ src/App.js
@@ -39,7 +39,6 @@ const useRoles = _ => {
             }
           }
         })
-        .then()
         .catch(error => {
           console.error(error)
           isMounted && setRoles([])
--- src/views/login.js
+++ src/views/login.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useEffect, useRef } from 'react'
 import styled from 'styled-components'
 import firebase from 'firebase'
 
@@ -7,24 +7,22 @@ import FlatButton from '../components/flat-button'
 
 const Login = (props = {}) => {
   const { className } = props
+  const isMountedRef = useRef(true)
   const [signInError, setSignInError] = useState(null)
   const [isAuthenticating, setIsAuthenticating] = useState(false)
 
-  const signIn = useCallback(async _ => {
-    let isMounted = true
+  useEffect(_ => _ => isMountedRef.current = false, [])
 
+  const signIn = useCallback(async _ => {
     setIsAuthenticating(true)
 
     await firebase.auth()
       .signInWithPopup(new firebase.auth.GoogleAuthProvider())
       .catch(error => {
         console.error(error)
-        isMounted && setIsAuthenticating(false)
-        isMounted && setSignInError(error)
+        isMountedRef.current && setSignInError(error)
       })
-      .then(_ => isMounted && setIsAuthenticating(false))
-
-    return _ => isMounted = false
+      .then(_ => isMountedRef.current && setIsAuthenticating(false))
   }, [])
 
   return (
