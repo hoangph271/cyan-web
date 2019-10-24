0025f446e6ab61f9dac501920bbb692d4645a73d
Phan Huy Hoang <hoang.phanhuy@vietis.com.vn>
2019-10-24 10:33:43 +0700

    🛠 Split search-box from search-artist-form

--- src/components/search-artist-form.js
+++ src/components/search-artist-form.js
@@ -1,70 +1,59 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import styled from 'styled-components'
 
 import { useInput } from '../utils/hooks'
 import { artistsCollection } from '../utils/firestore'
 
+import SearchBox from '../components/search-box'
 import ZenCircle from '../components/zen-circle'
-import IconedInput from '../components/iconed-input'
 import ArtistCard from '../components/artist-card'
 
-import search_white from '../assets/png/search_white.png'
-
 const RESULT_COUNT_LIMIT = 10
-const SEARCH_TIMEOUT_MS = 250
 
 const SearchArtistForm = (props = {}) => {
   const { className, resultLimit = RESULT_COUNT_LIMIT } = props
   const { onArtistClick = _ => {} } = props
 
   const [isSearching, setIsSearching] = useState(true)
-  const [keyword, onKeywordChange] = useInput('', { transformer: str => str.toLowerCase() })
+  const [keyword, , setKeyword] = useInput('', { transformer: str => str.toLowerCase() })
   const [artists, setArtists] = useState([])
+
+  const handleArtistSearch = useCallback(keyword => setKeyword(keyword), [setKeyword])
 
   useEffect(_ => {
     let isMounted = true
+    setIsSearching(true)
 
-    const debounceTimeout = setTimeout(_ => {
-      setIsSearching(true)
+    let firestoreQuery = artistsCollection()
+      .orderBy('title')
+      .limit(resultLimit)
 
-      let firestoreQuery = artistsCollection()
-        .orderBy('title')
-        .limit(resultLimit)
-
-      if (keyword) {
-        firestoreQuery = firestoreQuery.where('keywords', 'array-contains', keyword)
-      }
-
-      firestoreQuery
-        .get()
-        .then(snapshot => {
-          if (isMounted) {
-            const artists = snapshot.docs.map(doc => ({
-              id: doc.id,
-              ...doc.data(),
-            }))
-
-            setArtists(artists)
-            setIsSearching(false)
-          }
-        })
-        .catch(_ => setIsSearching(false))
-    }, SEARCH_TIMEOUT_MS)
-
-    return _ => {
-      clearTimeout(debounceTimeout)
-      isMounted = false
+    if (keyword) {
+      firestoreQuery = firestoreQuery.where('keywords', 'array-contains', keyword)
     }
+
+    firestoreQuery
+      .get()
+      .then(snapshot => {
+        if (isMounted) {
+          const artists = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+
+          setArtists(artists)
+          setIsSearching(false)
+        }
+      })
+      .catch(_ => setIsSearching(false))
+
+    return _ => isMounted = false
   }, [keyword, resultLimit])
 
   return (
     <div className={className}>
-      <IconedInput
-        value={keyword}
-        placeholder="Keyword"
-        className="search-box"
-        iconUrl={search_white}
-        onChange={onKeywordChange}
+      <SearchBox
+        onSearch={handleArtistSearch}
       />
       {isSearching ? (
         <ZenCircle />
---
+++ src/components/search-box.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import styled from 'styled-components'
+
+import { useInput } from '../utils/hooks'
+
+import IconedInput from '../components/iconed-input'
+
+import search_white from '../assets/png/search_white.png'
+
+const SEARCH_TIMEOUT_MS = 250
+
+const SearchBox = (props = {}) => {
+  const { className, onSearch } = props
+
+  const [keyword, onKeywordChange] = useInput('', { transformer: str => str.toLowerCase() })
+
+  useEffect(_ => {
+    const debounceTimeout = setTimeout(_ => {
+      onSearch(keyword)
+    }, SEARCH_TIMEOUT_MS)
+
+    return _ => clearTimeout(debounceTimeout)
+  }, [keyword, onSearch])
+
+  return (
+    <IconedInput
+      value={keyword}
+      placeholder="Keyword"
+      iconUrl={search_white}
+      onChange={onKeywordChange}
+      className={'search-box' + className || ''}
+    />
+  )
+}
+
+export default styled(SearchBox)`
+  
+`
