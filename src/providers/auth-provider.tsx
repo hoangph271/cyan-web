2f7bce8d6f027d6857f7259deb5d540232b4a04f
Phan Huy Hoàng <hoangph271@gmail.com>
2019-10-30 22:58:18 -0700

    🛠 FIX - httpsCallable claimRoles only when needed

--- src/providers/auth-provider.tsx
+++ src/providers/auth-provider.tsx
@@ -9,6 +9,16 @@ type AuthContextProps = {
 }
 const AuthContext = createContext<AuthContextProps>({} as AuthContextProps)
 
+const httpsCallableClaimRoles = async () => {
+  await firebase
+    .functions()
+    .httpsCallable('claimRoles')({})
+
+  await firebase
+    .auth()
+    .currentUser!.getIdToken(true)
+}
+
 type AuthProviderProps = { children?: ReactNode }
 const AuthProvider = (props: AuthProviderProps = {}) => {
   const { children } = props
@@ -21,41 +31,42 @@ const AuthProvider = (props: AuthProviderProps = {}) => {
 
     firebase.auth()
       .onAuthStateChanged(userInfo => {
-      if (userInfo === null) {
-        isMounted && setRoles([])
-        return
-      }
 
-      rolesCollection()
-        .doc(userInfo.uid)
-        .get()
-        .then(doc => {
-          if (isMounted) {
-            const { isActive, roles } = doc.data() || {}
+        if (userInfo === null) {
+          console.info('Not logged in...!')
+          return
+        }
 
-            setRoles(isActive ? roles : [])
+        rolesCollection()
+          .doc(userInfo.uid)
+          .get()
+          .then(async doc => {
+            if (isMounted) {
+              const { isActive, roles } = doc.data() || {}
+              const { claims } = await userInfo.getIdTokenResult()
 
-            if (Array.isArray(roles)) {
-              firebase.functions()
-                .httpsCallable('claimRoles')({})
-                .then(_ => userInfo.getIdToken(true))
+              const isRolesNOTClaimed = roles && !Array.isArray(claims.roles)
+              if (isRolesNOTClaimed) {
+                 await httpsCallableClaimRoles()
+              }
+
+              setRoles(isActive ? roles : [])
             }
-          }
+          })
+          .catch(error => {
+            console.error(error)
+            isMounted && setRoles([])
+          })
         })
-        .catch(error => {
-          console.error(error)
-          isMounted && setRoles([])
-        })
-      })
 
-      return () => { isMounted = false }
+    return () => { isMounted = false }
   }, [])
   useEffect(() => {
     let isMounted = true
 
     firebase
       .auth()
-      .onAuthStateChanged((userInfo) => isMounted && setUser(userInfo))
+      .onAuthStateChanged(userInfo => isMounted && setUser(userInfo))
 
     return () => { isMounted = false }
   }, [])
